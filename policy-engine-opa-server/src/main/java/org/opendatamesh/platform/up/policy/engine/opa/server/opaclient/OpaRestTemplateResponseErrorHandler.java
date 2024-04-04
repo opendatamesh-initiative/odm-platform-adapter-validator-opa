45817ec8d8c7070b66cf7bc5193467f0cd664ba3
Mattia Pennati <mattia.pennati@quantyca.it>
2024-04-04 13:10:51 +0200

    Parse OPA error responses

--- policy-engine-opa-server/src/main/java/org/opendatamesh/platform/up/policy/engine/opa/server/opaclient/OpaRestTemplateResponseErrorHandler.java
+++ policy-engine-opa-server/src/main/java/org/opendatamesh/platform/up/policy/engine/opa/server/opaclient/OpaRestTemplateResponseErrorHandler.java
@@ -1,7 +1,11 @@
 package org.opendatamesh.platform.up.policy.engine.opa.server.opaclient;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
 import org.opendatamesh.platform.core.commons.servers.exceptions.BadRequestException;
 import org.opendatamesh.platform.core.commons.servers.exceptions.InternalServerException;
+import org.opendatamesh.platform.core.dpds.ObjectMapperFactory;
 import org.opendatamesh.platform.up.policy.engine.opa.server.resources.errors.PolicyEngineOpaErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
@@ -10,9 +14,12 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResponseErrorHandler;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class OpaRestTemplateResponseErrorHandler implements ResponseErrorHandler {
+
+    private ObjectMapper objectMapper = ObjectMapperFactory.JSON_MAPPER;
 
     @Override
     public boolean hasError(ClientHttpResponse httpResponse) throws IOException {
@@ -22,22 +29,27 @@ public class OpaRestTemplateResponseErrorHandler implements ResponseErrorHandler
 
     @Override
     public void handleError(ClientHttpResponse httpResponse) throws IOException {
+        String responseBodyString = "no message available";
+        if (httpResponse.getBody() != null) {
+            JsonNode responseBody = objectMapper.readTree(httpResponse.getBody());
+            responseBodyString = responseBody.toString();
+        }
         if (httpResponse.getRawStatusCode() == 503) {
             throw new InternalServerException(
                     PolicyEngineOpaErrors.SC500_02_OPA_SERVER_NOT_REACHABLE,
-                    httpResponse.getBody().toString()
+                    responseBodyString
             );
         } else if (httpResponse.getStatusCode().is5xxServerError()) {
             //Handle SERVER_ERROR
             throw new InternalServerException(
                     PolicyEngineOpaErrors.SC500_01_OPA_SERVER_INTERNAL_SERVER_ERROR,
-                    httpResponse.getBody().toString()
+                    responseBodyString
             );
         } else if (httpResponse.getStatusCode().is4xxClientError()) {
             //Handle CLIENT_ERROR
             throw new BadRequestException(
                     PolicyEngineOpaErrors.SC400_OPA_SERVER_BAD_REQUEST,
-                    httpResponse.getBody().toString()
+                    responseBodyString
             );
         }
     }
